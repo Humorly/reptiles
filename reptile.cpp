1c6721f63f84b133d60cc579ddb9eff07f7bc002
陈安卓 <chenanzhuo517@gmail.com>
2020-09-17 11:31:50 +0800

    案例重置

--- reptile.cpp
+++ reptile.cpp
@@ -1,18 +1,28 @@
 // reptile.cpp: 定义控制台应用程序的入口点。
 //
 
+#include "pch.h"
+
 #include <memory>
 #include "spider.h"
 
 int main()
 {
-	//std::shared_ptr<spider> spider_http_(new http_spider());
-	//spider_http_->get_content("http://www.gx8899.com/");
+	std::string str_net_;
+	std::cout << "请输入网址:\n";
+	std::cin >> str_net_;
 
-	std::shared_ptr<spider> spider_https_(new https_spider());
-	spider_https_->get_content("https://www.qqtn.com/tx/weixintx_1.html");
+	if (std::string::npos != str_net_.find("https"))
+	{
+		std::shared_ptr<spider> spider_https_(new https_spider());
+		spider_https_->get_content(str_net_);
+	}
+	else if (std::string::npos != str_net_.find("http"))
+	{
+		std::shared_ptr<spider> spider_http_(new http_spider());
+		spider_http_->get_content(str_net_);
+	}
 
 	while (true);
 	return 0;
 }
- 
